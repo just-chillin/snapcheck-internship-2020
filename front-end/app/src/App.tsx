857e02342d065bf3e1ec2269bb9a4dc15fb87a08
corrina <csivak345@gmail.com>
2021-03-10 00:10:34 -0500

    ayyyyy got dark mode working

--- front-end/app/src/App.tsx
+++ front-end/app/src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {DBView} from './components/DBView';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { DBView } from "./components/DBView";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
 function App() {
-  return <DBView/>
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="DBView">
+        <DBView />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- front-end/app/src/components/DBView.tsx
+++ front-end/app/src/components/DBView.tsx
@@ -1,5 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { DataGrid, GridCellParams, GridColDef } from "@material-ui/data-grid";
+import { CircularProgress } from "@material-ui/core";
+
+function addressFromGridCellParams(params: GridCellParams): string {
+  let { address1, address2, city, state, zip } = params.row.address;
+  console.debug(params.row);
+  // If addr2 is falsy, set it to an empty string. Otherwise, left pad it one space.
+  address2 = address2 ? " " + address2 : "";
+
+  return `${address1}${address2}, ${city}, ${state} ${zip}`;
+}
+
+const columns: GridColDef[] = [
+  { field: "first_name", headerName: "First Name", width: 150 },
+  { field: "last_name", headerName: "Last Name", width: 150 },
+  {
+    field: "address",
+    headerName: "Address",
+    valueGetter: addressFromGridCellParams,
+    width: 300
+  },
+  { field: "gender", headerName: "Gender" },
+  { field: "age", headerName: "Age" },
+  {
+    field: "amount",
+    headerName: "Amount",
+    width: 150,
+    valueGetter: (params) => params.row.order_total.amount
+  },
+  {
+    field: "currency",
+    headerName: "Currency",
+    width: 150,
+    valueGetter: (params) => params.row.order_total.currency
+  },
+];
+
+interface Order {
+  id: number;
+  first_name: string;
+  last_name: string;
+  address: {
+    address1: string;
+    address2: string;
+    city: string;
+    state: string;
+    zip: string;
+  };
+  gender: string;
+  age: number;
+  order_total: {
+    currency: string;
+    amount: number;
+  };
+}
+
+async function getOrders(): Promise<Order[]> {
+  const result = await fetch("http://localhost:3001/people");
+  const orders = (await result.json()) as Order[];
+  console.log(orders);
+  return orders;
+}
 
 export function DBView() {
-    return <></>;
+  const [orders, setOrders] = useState<Order[]>();
+  useEffect(() => {
+    getOrders().then(setOrders);
+  }, []);
+
+  if (!orders) {
+    return <CircularProgress />;
+  }
+  return <DataGrid columns={columns} rows={orders} autoPageSize />;
 }
