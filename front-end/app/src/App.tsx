9a75e0ae19dc090127cc4f2050e0b37dfa0a2e74
Corrina Sivak <csivak345@gmail.com>
2021-03-14 16:31:23 -0400

    auth works

--- front-end/app/src/App.tsx
+++ front-end/app/src/App.tsx
@@ -3,7 +3,8 @@ import "./App.css";
 import { DBView } from "./DBView";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import { CssBaseline } from "@material-ui/core";
-import { Route, Switch } from "react-router";
+import { Route, Switch, Router, useHistory } from "react-router";
+import { createBrowserHistory } from "history";
 import { AuthPage } from "./AuthPage";
 const darkTheme = createMuiTheme({
   palette: {
@@ -11,16 +12,28 @@ const darkTheme = createMuiTheme({
   },
 });
 
+function Navigation() {
+  const history = createBrowserHistory();
+  return (
+    <Router history={history}>
+      <DBView />
+      <Route exact path="/new"></Route>
+      <Route exact path="/edit/:id"></Route>
+      <Route exact path="/delete/:id"></Route>
+    </Router>
+  );
+}
+
 /**
  * Component that conditionally renders it's children depending on whether or
  * not valid credentials are found in local storage.
  */
-function AuthRequired({children}: React.PropsWithChildren<{}>) {
+function AuthRequired({ children }: React.PropsWithChildren<{}>) {
   const is_authenticated = localStorage.getItem("auth_token") !== null;
   if (is_authenticated) {
     return <>{children}</>;
   } else {
-    return <AuthPage />
+    return <AuthPage />;
   }
 }
 
@@ -29,7 +42,7 @@ export default function App() {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <AuthRequired>
-        <DBView />
+        <Navigation></Navigation>
       </AuthRequired>
     </ThemeProvider>
   );
--- front-end/app/src/AuthPage.tsx
+++ front-end/app/src/AuthPage.tsx
@@ -1,5 +1,92 @@
-import React from 'react';
+import { Button, Paper, TextField, Typography } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import React, { useState } from "react";
+import { useHistory, useLocation } from "react-router";
+
+async function storeCredentials(email: string, password: string) {
+  // Encrypt the credentials.
+  // Precautionary, you still shouldn't use real credentials to log in here.
+  const raw = await crypto.subtle.digest(
+    "SHA-256",
+    new TextEncoder().encode(`${email}${password}`)
+  );
+
+  // Decode the encrypted buffer
+  const auth_token = new TextDecoder().decode(raw);
+
+  // Set the auth_token key in localStorage
+  localStorage.setItem("auth_token", auth_token);
+}
+
+function AuthHeader() {
+  return (
+    <Grid item>
+      <Typography variant="h3">Please Log In</Typography>
+    </Grid>
+  );
+}
+
+async function submitCredentials() {
+  console.log("submitting");
+  await storeCredentials("fake", "credentials");
+  window.location.reload();
+}
+
+function AuthForm() {
+  return (
+    <form>
+      <Grid item>
+        <TextField
+          required
+          label="Email"
+          type="email"
+          autoComplete="username"
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          required
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
+      </Grid>
+      <Grid item>
+        <Button variant="contained" onClick={submitCredentials}>Authenticate</Button>
+      </Grid>
+    </form>
+  );
+}
+
+function AuthGrid() {
+  return (
+    <Paper style={{ padding: "3em" }}>
+      <Grid container direction="column" justify="center" alignItems="center">
+        <AuthHeader />
+        <AuthForm />
+      </Grid>
+    </Paper>
+  );
+}
 
 export function AuthPage() {
-    return <p>Pls authenticate</p>;
+  return (
+    <div
+      style={{
+        height: "100%",
+        margin: 0,
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <AuthGrid />
+      </div>
+    </div>
+  );
 }
